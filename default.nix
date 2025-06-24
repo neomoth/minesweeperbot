8a2dc57bce8bc3a77a213575f90b48f61c289585
neomoth <69372252+neomoth@users.noreply.github.com>
2025-06-24 12:53:54 -0400

    nginx attempt number 15

--- default.nix
+++ default.nix
@@ -1,63 +1,19 @@
-{ pkgs ? import <nixpkgs> {} }:
+{lib,buildNpmPackage,fetchFromGithub,}:
 
-let
-  # Get this hash by:
-  # 1. First run with fake hash (sha256-AAAAAAAA...)
-  # 2. Copy the actual hash from the error message
-  npmDepsHash = "sha256-8kHZmODo5El+WxWk1XG383mapgJPRODGOkbmEyqOB5M="; # REPLACE THIS
-in
-pkgs.stdenv.mkDerivation rec {
-  pname = "minesweeperbot";
-  version = "1.0.0";
-  src = ./.;
+buildNpmPackage(finalAttrs: {
+	pname="minesweeperbot";
+	version="1.0.0";
 
-  npmDeps = pkgs.fetchNpmDeps {
-    src = src;
-    hash = npmDepsHash;
-  };
+	src = ./.;
 
-  nativeBuildInputs = [
-    pkgs.nodejs_22
-  ];
+	npmDepsHash="sha256-8kHZmODo5El+WxWk1XG383mapgJPRODGOkbmEyqOB5M=";
 
-  configurePhase = ''
-    runHook preConfigure
-    export HOME=$(mktemp -d)
-    runHook postConfigure
-  '';
+	npmPackFlags = ["--ignore-scripts"];
 
-  buildPhase = ''
-    runHook preBuild
+	NODE_OPTIONS = "--openssl-legacy-provider";
 
-    # Create and populate node_modules from pre-fetched deps
-    mkdir -p node_modules
-    cp -r $npmDeps/* node_modules/
-    chmod -R +w node_modules
-
-    # Verify the lockfile matches the pre-fetched deps
-    npm ci --omit=dev --offline --no-audit --no-fund --ignore-scripts
-
-    runHook postBuild
-  '';
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/libexec/${pname}
-    cp -r ./* $out/libexec/${pname}/
-
-    mkdir -p $out/bin
-    cat <<EOF > $out/bin/${pname}
-#!/bin/sh
-exec ${pkgs.nodejs_22}/bin/node $out/libexec/${pname}/index.js
-EOF
-    chmod +x $out/bin/${pname}
-
-    runHook postInstall
-  '';
-
-  meta = {
-    description = "minesweeper bot for OWOP";
-    platforms = pkgs.lib.platforms.linux;
-  };
-}
+	meta = {
+		description = "minesweeper bot for OWOP";
+        platforms = pkgs.lib.platforms.linux;
+	};
+})
