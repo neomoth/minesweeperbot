aa7aa05f5827c15df85fedf311150cda6deee809
neomoth <69372252+neomoth@users.noreply.github.com>
2025-06-24 01:14:29 -0400

    nginx attempt number 9

--- default.nix
+++ default.nix
@@ -1,42 +1,43 @@
-{ pkgs ? import<nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {} }:
 
 with pkgs; stdenv.mkDerivation rec {
-	pname = "minesweeperbot";
-	version = "1.0.0";
-	src = ./.;
+  pname = "minesweeperbot";
+  version = "1.0.0";
+  src = ./.;
 
-	serverRuntime = pkgs.nodejs_22;
+  serverRuntime = pkgs.nodejs_22;
 
-	nativeBuildInputs = [
-		pkgs.nodejs_22
-		npmHooks.npmInstallHook
-	];
+  nativeBuildInputs = [
+    pkgs.nodejs_22
+    npmHooks.npmInstallHook
+  ];
 
-	npmDeps = fetchNpmDeps {
-		src= ./.;
-		hash="sha256-CHANGEME";
-	};
+  npmDeps = fetchNpmDeps {
+    src = ./.;
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # Temporary placeholder
+  };
 
-	buildPhase = ''
-		export HOME=$(mktemp -d)
-		mkdir -p $out/bin $out/libexec/${pname}
-		cp -r ./* $out/libexec/${pname}/
-		cd $out/libexec/${pname}/
-		export NPM_CONFIG_FUND=false
-		export NPM_CONFIG_AUDIT=false
-		npm ci --omit=dev
-	'';
+  buildPhase = ''
+    export HOME=$(mktemp -d)
+    mkdir -p $out/libexec/${pname}
+    cp -r ./* $out/libexec/${pname}/
+    cd $out/libexec/${pname}/
+    export NPM_CONFIG_FUND=false
+    export NPM_CONFIG_AUDIT=false
+    npm ci --omit=dev --offline
+  '';
 
-	installPhase = ''
-		cat <<EOF > $out/libexec/${pname}
+  installPhase = ''
+    mkdir -p $out/bin
+    cat <<EOF > $out/bin/${pname}
 #!/bin/sh
-exec ${serverRuntime}/bin/node '$out/libexec/${pname}/index.js'
+exec ${serverRuntime}/bin/node $out/libexec/${pname}/index.js
 EOF
-		chmod +x $out/bin/${pname}
-	'';
+    chmod +x $out/bin/${pname}
+  '';
 
-	meta = {
-		description="minesweeper bot for OWOP";
-		platforms = pkgs.lib.platforms.linux;
-	};
+  meta = {
+    description = "minesweeper bot for OWOP";
+    platforms = pkgs.lib.platforms.linux;
+  };
 }
