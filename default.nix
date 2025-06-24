f09a0d4e8f9fd78fc37f3fb480fcd68993f2d38d
neomoth <69372252+neomoth@users.noreply.github.com>
2025-06-24 13:11:59 -0400

    nginx attempt number 23

--- default.nix
+++ default.nix
@@ -1,50 +1,28 @@
-let
-  pkgs = import <nixpkgs> {};
-in
+{ pkgs, hooks, nodejs }:
+
 pkgs.buildNpmPackage {
   pname = "minesweeperbot";
   version = "1.0.0";
-
   src = ./.;
 
-  npmDepsHash = "sha256-oiZFqvbVqXZTjpcmZee+7YGjjF+5ZaUg3ciKVpPigNk=";
+  inherit nodejs;
 
-  npmPackFlags = ["--ignore-scripts"];
-  dontNpmBuild = true;
-
-  # This ensures that node-gyp can build native modules
-  nativeBuildInputs = with pkgs; [
-    pkg-config
-    python3
-    nodejs_22
-  ];
-
-  buildInputs = with pkgs; [
-    cairo
-    libpng
-    pango
-    pixman
-    giflib
-    glib
-    zlib
-  ];
-
-  # Some modules still want this for compatibility
-  env = {
-    PKG_CONFIG_PATH = pkgs.lib.makeSearchPath "lib/pkgconfig" [
-      pkgs.cairo
-      pkgs.libpng
-      pkgs.pango
-      pkgs.pixman
-      pkgs.giflib
-      pkgs.glib
-      pkgs.zlib
-    ];
-    NODE_OPTIONS = "--openssl-legacy-provider";
+  npmDeps = pkgs.importNpmLock {
+    npmRoot = ./.;
   };
 
-  meta = {
-    description = "minesweeper bot for OWOP";
-    license = pkgs.lib.licenses.mit;
-  };
+  preConfigure = hooks.prepare;
+
+  postBuild = ''
+    npx next-sitemap
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    npx pagefind --site ./out
+    mv out $out
+    runHook postInstall
+  '';
+
+  npmConfigHook = pkgs.importNpmLock.npmConfigHook;
 }
