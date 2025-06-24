317ad80fdae9dd321d0a58ba7689f499916018ee
neomoth <69372252+neomoth@users.noreply.github.com>
2025-06-24 16:52:36 -0400

    nix.

--- default.nix
+++ default.nix
@@ -1,43 +1,44 @@
 { nixpkgs ? import <nixpkgs> {  } }:
 with nixpkgs; buildNpmPackage rec {
-	name = "minesweeperbot";
-	npmDepsHash = "sha256-oiZFqvbVqXZTjpcmZee+7YGjjF+5ZaUg3ciKVpPigNk=";
+        name = "minesweeperbot";
+        npmDepsHash = "sha256-oiZFqvbVqXZTjpcmZee+7YGjjF+5ZaUg3ciKVpPigNk=";
 
-	src = fetchGit {
-		url = ./.;
-	};
+        src = fetchGit {
+                url = ./.;
+        };
 
-	enableParallelBuilding = true;
-	nodejs = pkgs.nodejs_20;
-	nativeBuildInputs = [
-		python3
-		node-gyp
-		nodejs
-		pkg-config
-		pixman
-		#npmHooks.npmInstallHook
-	];
+        enableParallelBuilding = true;
+        nodejs = pkgs.nodejs_20;
+        nativeBuildInputs = [
+                python3
+                node-gyp
+                nodejs
+                pkg-config
+                #npmHooks.npmInstallHook
+        ];
 
-	# Grab the dependencies for running later
-	buildPhase = ''
-		mkdir -p $out/bin $out/libexec/${name}
-		cp package.json $out/libexec/${name}/
+        buildInputs = [ pixman cairo pango ];
 
-		cp -r ./ $out/libexec/${name}/
-	'';
-	#cp -r node_modules $out/libexec/${name}/
+        # Grab the dependencies for running later
+        buildPhase = ''
+                mkdir -p $out/bin $out/libexec/${name}
+                cp package.json $out/libexec/${name}/
 
-	# Write a script to the output folder that invokes the entrypoint of the application
-	installPhase = ''
-		cat <<EOF > $out/bin/${name}
+                cp -r ./ $out/libexec/${name}/
+        '';
+        #cp -r node_modules $out/libexec/${name}/
+
+        # Write a script to the output folder that invokes the entrypoint of the application
+        installPhase = ''
+                cat <<EOF > $out/bin/${name}
 #!${runtimeShell}
 exec ${nodejs}/bin/node '$out/libexec/${name}/index.js';
 EOF
-		chmod a+x $out/bin/${name}
-	'';
+                chmod a+x $out/bin/${name}
+        '';
 
-	meta = {
-		description = "minesweeper bot for owop";
-		platforms = lib.platforms.linux;
-	};
+        meta = {
+                description = "minesweeper bot for owop";
+                platforms = lib.platforms.linux;
+        };
 }
