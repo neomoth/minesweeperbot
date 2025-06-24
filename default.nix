d0d6ab225bca6b4dd880dfce771e33c195c3649f
neomoth <69372252+neomoth@users.noreply.github.com>
2025-06-24 13:02:13 -0400

    nginx attempt number 18

--- default.nix
+++ default.nix
@@ -1,21 +1,19 @@
-{
-	lib ? import <nixpkgs> {},
-	buildNpmPackage,
-}:
+let
+  pkgs = import <nixpkgs> {};
+in
+pkgs.buildNpmPackage {
+  pname = "minesweeperbot";
+  version = "1.0.0";
 
-buildNpmPackage(finalAttrs: {
-	pname="minesweeperbot";
-	version="1.0.0";
+  src = ./.;
 
-	src = ./.;
+  npmDepsHash = "sha256-8kHZmODo5El+WxWk1XG383mapgJPRODGOkbmEyqOB5M=";
 
-	npmDepsHash="sha256-8kHZmODo5El+WxWk1XG383mapgJPRODGOkbmEyqOB5M=";
+  npmPackFlags = ["--ignore-scripts"];
 
-	npmPackFlags = ["--ignore-scripts"];
+  NODE_OPTIONS = "--openssl-legacy-provider";
 
-	NODE_OPTIONS = "--openssl-legacy-provider";
-
-	meta = {
-		description = "minesweeper bot for OWOP";
-	};
-})
+  meta = {
+    description = "minesweeper bot for OWOP";
+  };
+}
